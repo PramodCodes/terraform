df39af5d921cb0140ad9cd01fadec81f9950fb24
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-12-16 18:50:27 +0530

    udpate

--- locals/provider.tf
+++ locals/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.81.0" 
+      version = "5.31.0" # AWS provider version, not terraform version
     }
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
--- locals/variables.tf
+++ locals/variables.tf
@@ -1,46 +1,16 @@
 variable "instance_names" {
-    type = list
-    default = [ "mongodb","web","redis","user","mysql","rabbitmq","catalogue","cart","shipping","payment","dispatch" ]
+  type = list
+  default = ["mongodb","redis","mysql","rabbitmq","catalogue","user","cart","shipping","payment","dispatch","web"]
 }
 
 variable "ami_id" {
-    type = string
-    default = "ami-0b4f379183e5706b9"
+  default = "ami-0b4f379183e5706b9"
 }
 
-
-variable "instance_name" {
-    type = string
-    default = "MongoDB"
-}
-# tags are maps so 
-
-variable "instance_t2-micro" {
-    type = string
-    default = "t2.micro"
-}
-
-variable "instance_t3-small" {
-    type = string
-    default = "t3.small"
-}
-
-variable "tags" {
-    default = {
-        Project = "roboshop"
-        Environment = "dev"
-        Component = "web"
-        Terraform  = "true"
-    }
-}
-
-variable "r53-zone_id" {
-    type = string
-    default = "Z056232231439EYIBQD0B"
+variable "zone_id" {
+  default = "Z056232231439EYIBQD0B"
 }
 
 variable "domain_name" {
-    type = string
-    default = "pka.in.net"
-}   
- 
+  default = "pka.in.net"
+}

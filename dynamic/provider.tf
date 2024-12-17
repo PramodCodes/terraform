9cb08d9cb99e8f27ac9b0941b075aa7ba368b7c1
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2024-12-17 21:25:13 +0530

    Update provider.tf

--- dynamic/provider.tf
+++ dynamic/provider.tf
@@ -5,8 +5,15 @@ terraform {
       version = "5.81.0" 
     }
   }
+    backend "s3" {
+    bucket = "pka.in.net"
+    key    = "pka-tfstateKey"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state"
+  }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
+
